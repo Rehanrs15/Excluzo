8e6f1e46b363ba2c36e00e946ee58f015b1e4ebb
syed <syedmunawwar990@gmail.com>
2016-08-24 17:56:54 +0530

    Added Base Classes for Activities,Fragments and Color changes

---
+++ app/src/main/java/com/examples/rehan/excluzo/Activities/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.examples.rehan.excluzo.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rehan r on 24-08-2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/examples/rehan/excluzo/Fragments/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.examples.rehan.excluzo.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by rehan r on 24-08-2016.
+ */
+public class BaseFragment extends Fragment {
+
+
+}
